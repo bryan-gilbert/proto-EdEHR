e20b8ccda1a446ed5f56de5b9d7fb9d703e2f4a8
Bryan Gilbert <bryan.gilbert@memsharp.com>
2018-09-25 10:36:33 -0700

    wip markdown
    wip markdown
    
    
    wip
    
    
    wip
    
    
    Scrivener wip
    
    
    remove markdown list marker
    
    because i found the way to get scrivener to make markdown lits
    wip docs
    
    
    docs
    
    
    roadmap docs
    
    
    update docs
    
    
    prototype ddd
    
    
    Merge remote-tracking branch 'origin/master'

--- markdown-toc.js
+++ markdown-toc.js
@@ -12,7 +12,6 @@ const scrivenerOutputDir = path.resolve(scrivenerDir, 'output');
 let globOptions = {
   cwd: scrivenerDir
 }
-console.log("Looking for Scrivener markdown output files in ", scrivenerDir)
 glob("**/*.md", globOptions, function (er, files) {
   console.log('found:', files);
   files.forEach(f => {
@@ -25,20 +24,30 @@ glob("**/*.md", globOptions, function (er, files) {
     }
   })
 })
+console.log("Looking for Scrivener markdown output files in ", scrivenerDir)
 
 function processMarkdownDir(dir) {
+  /*
+  The idea is to move any Scrivener exported images into the docs directory but this
+  is not working. Perhaps because Scrivener saves embeded images as tiffs or perhaps
+  because of some relative path problem.
+  Leaving this code here in case someone wants to get it to work.
+   */
   console.log('Search directory for images', dir)
   glob("**/*.tiff", {cwd: dir}, function (er, files) {
     console.log('found TIFFS:', files);
     files.forEach(f => {
       let src = path.resolve(dir,f);
       let dst = path.resolve('docs',f);
+      console.log("Not rename ", src, dst)
+      /*
       fs.rename(src,dst, function(err,stats){
         if(err) {
           console.err(err);
           return;
         }
       })
+      */
     })
   })
 }
@@ -56,22 +65,27 @@ function processMarkdownFile(f) {
       let m = r.exec(aLine);
       if (m) {
         if (insertIndex < 0) {
+          // this is where we'll insert the TOC, just above the first header
           insertIndex = inx
         }
-        let level = m[1].length;
-        let title = m[2].trim();
+        let level = m[1].length; // the ###
+        let title = m[2].trim(); // the title text
+        // remove all punctuation
+        // note to ready this code for production we should remove everything but alpha
         let key = title.replace(/([!@#$%^&*(),\.:;"'{}[\]_\-\+=]+)/g, '')
+        // prepend "user content" because GitHub markdown will make links with this prefix
+        // for each header element. Links are lower case
         key = 'user content ' + key.toLowerCase();
+        // spaces are replaced with hyphens
         key = key.replace(/\s/g, '-');
-        //console.log(key);
-        // ### <a name="tith"></a>This is the Heading
+        // sample result
         // user-content-planning--management
-
-
-        //let newLine = m[1] + ' <a id="#'+key+'"></a>' + title + ' ' + m[1];
-        lines[inx] = m[0] + '\n<a href="#toc">Top</a>\n';
-        let link = ' '.repeat(level - 1) + '* [' + title + '](#' + key + ')'
+        // indent the links to reflect the document structure
+        let link = ' '.repeat((level-1)*2) + '* [' + title + '](#' + key + ')'
         toc.push(link);
+
+        // insert a "back to top" just after the header
+        lines[inx] = m[0] + '\n<a href="#toc">Top</a>\n';
       }
     })
     toc.push(' ');
@@ -81,7 +95,6 @@ function processMarkdownFile(f) {
     let newContents = lines.join('\n')
     console.log("output in ", dst)
     fs.writeFileSync(dst, newContents)
-    process.exit();
   })
 
   //fs.copyFileSync(src,dst)
