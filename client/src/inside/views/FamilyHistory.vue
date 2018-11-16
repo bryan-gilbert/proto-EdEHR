99df53666c443ecb78eaed313ab30bbc18da7160
Bryan Gilbert <bryan.gilbert@memsharp.com>
2018-11-16 06:34:28 -0800

    Clean up generated VUE files

--- client/src/inside/views/FamilyHistory.vue
+++ client/src/inside/views/FamilyHistory.vue
@@ -1,13 +1,13 @@
 // Generated VUE file. Before modifying see docs about Vue file generation 
 <template lang="pug">
   div(:class="$options.name")
-    h1(slot="ehrPageTitle") Non-medication orders: Orders
+    h1(slot="ehrPageTitle") Family History
     div(class="region")
-      p component name: NoMedOrders
-      p title: Non-medication orders: Orders
+      p component name: FamilyHistory
+      p title: Family History
       p redirect: 
-      p route name: no-med-orders
-      p full path: /ehr/current/no-med/no-med-orders
+      p route name: family-history
+      p full path: /ehr/patient/history/family-history
       p another line now using spaces
     div(class="region")
       div(class="columns")
@@ -38,7 +38,7 @@
 
 <script>
 export default {
-  name: 'NoMedOrders'
+  name: 'FamilyHistory'
 }
 </script>
 
@@ -52,7 +52,7 @@ select {
 .region {
   /* border: 1px dashed black; */
 }
-.NoMedOrders {
+.FamilyHistory {
   padding: 1rem;
 
   &__main {
--- client/src/inside/views/Respiratory.vue
+++ client/src/inside/views/Respiratory.vue
@@ -1,7 +1,39 @@
+// Generated VUE file. Before modifying see docs about Vue file generation 
 <template lang="pug">
- div(:class="$options.name")
-  h1(slot="ehrPageTitle") {{$options.name}}
-   p Respiratory
+  div(:class="$options.name")
+    h1(slot="ehrPageTitle") Respiratory
+    div(class="region")
+      p component name: Respiratory
+      p title: Respiratory
+      p redirect: 
+      p route name: respiratory
+      p full path: /ehr/current/assessments/respiratory
+      p another line now using spaces
+    div(class="region")
+      div(class="columns")
+        div(class="column is-one-third")
+          label(for="sampleTA") Sample Textarea
+          textarea(class="textarea" id="sampleTA", name="Sample TS")
+        div(class="column is-one-third")
+          label column two
+          input(class="input is-info" readonly value="read only text" type="text" placeholder="Text input")
+        div(class="column is-one-third")
+          label column three
+          input(class="input is-info" readonly value="read only text" type="text" placeholder="Text input")
+      div(class="columns")
+        div(class="column is-one-third")
+          label another
+          input(class="input", type="text")
+        div(class="column is-one-third")
+          label another
+          input(class="input", type="text")
+        div(class="column is-one-third")
+          label pick one
+          div(class="select")
+            select
+              option one
+              option two
+              option three
 </template>
 
 <script>
@@ -11,8 +43,20 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.select {
+  display: block;
+}
+select {
+  width: 100%;
+}
+.region {
+  /* border: 1px dashed black; */
+}
 .Respiratory {
+  padding: 1rem;
+
   &__main {
+    font-size: 0.8rem;
   }
 }
 </style>
