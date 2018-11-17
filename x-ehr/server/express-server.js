c2f032ac3cc21a2dadfcb807130d7d477e502ab0
Bryan Gilbert <bryan.gilbert@memsharp.com>
2018-11-17 08:00:35 -0800

    Put all x-* into tar
    This removes all the obsolete code yet preserves the code in case something is needed later.

--- x-ehr/server/express-server.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const app = express()
-app.get('/api', (req, res) => {
-  res.send({
-    message: 'I am a server route and can also be hot reloaded!'
-  })
-})
-export default app
