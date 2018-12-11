673e58fc5bc1f9ce551c95edc744b5588077bee2
Bryan Gilbert <bryan.gilbert@memsharp.com>
2018-12-11 10:08:58 -0800

    demographics assignment 2 seed data

--- client/src/inside/views/Demographics.vue
+++ client/src/inside/views/Demographics.vue
@@ -7,115 +7,115 @@
         div(class="columns")
           div(class="column is-one-third")
             label(for="familyName") Last name
-            input(class="input", name="familyName",v-model="input.familyName")
+            input(class="input", name="familyName",v-model="demographics.familyName")
           div(class="column is-one-third")
             label(for="givenName") First name
-            input(class="input", name="givenName",v-model="input.givenName")
+            input(class="input", name="givenName",v-model="demographics.givenName")
           div(class="column is-one-third")
             label(for="middleName") Middle name(s)
-            input(class="input", name="middleName",v-model="input.middleName")
+            input(class="input", name="middleName",v-model="demographics.middleName")
         div(class="columns")
           div(class="column is-one-third")
             label(for="preferredName") Preferred name
-            input(class="input", name="preferredName",v-model="input.preferredName")
+            input(class="input", name="preferredName",v-model="demographics.preferredName")
           div(class="column is-one-third")
             label(for="dateOfBirth") Date of birth
-            input(class="input", name="dateOfBirth",v-model="input.dateOfBirth")
+            input(class="input", name="dateOfBirth",v-model="demographics.dateOfBirth")
           div(class="column is-one-third")
             label(for="personAge") Age
-            input(class="input", name="personAge",v-model="input.personAge")
+            input(class="input", name="personAge",v-model="demographics.personAge")
         div(class="columns")
           div(class="column is-one-third")
-            label(for="gender") Gender {{input.gender}}
+            label(for="gender") Gender {{demographics.gender}}
             div(class="select")
-              select(name="gender",v-model="input.gender")
+              select(name="gender",v-model="demographics.gender")
                 option(v-for="option in genders", v-bind:value="option.value") {{ option.text}}
           div(class="column is-one-third")
             label(for="martialStatus") Martial Status
             div(class="select")
-              select(name="martialStatus",v-model="input.martialStatus")
+              select(name="martialStatus",v-model="demographics.martialStatus")
                 option(v-for="option in mStatuses", v-bind:value="option.value") {{ option.text}}
           div(class="column is-one-third")
             label(for="codeStatus") Code status
             div(class="select")
-              select(name="codeStatus",v-model="input.codeStatus")
+              select(name="codeStatus",v-model="demographics.codeStatus")
                 option(v-for="option in codeStatuses", v-bind:value="option.value") {{ option.text}}
         div(class="columns")
           div(class="column is-one-third")
             label(for="languagePrimary") Primary language
             div(class="select")
-              select(name="languagePrimary",v-model="input.languagePrimary")
+              select(name="languagePrimary",v-model="demographics.languagePrimary")
                 option(v-for="option in languages", v-bind:value="option.value") {{ option.text}}
           div(class="column is-one-third")
             label(for="religion") Religion
-            input(class="input", name="religion",v-model="input.religion")
+            input(class="input", name="religion",v-model="demographics.religion")
           div(class="column is-one-third")
             label(for="indigenous") Do you identify as an indigenous person?
-            div(class="select", name="indigenous",v-model="input.indigenous")
+            div(class="select", name="indigenous",v-model="demographics.indigenous")
               select
                 option(value='no') No
                 option(value='yes') Yes
         div(class="columns")
           div(class="column is-two-third")
             label(for="streetAddress") Street address
-            input(class="input", name="streetAddress",v-model="input.streetAddress")
+            input(class="input", name="streetAddress",v-model="demographics.streetAddress")
           div(class="column is-one-third")
             label(for="sity") City
-            input(class="input", name="city",v-model="input.city")
+            input(class="input", name="city",v-model="demographics.city")
         div(class="columns")
           div(class="column is-one-third")
             label(for="country") Country
-            input(class="input", name="country",v-model="input.country")
+            input(class="input", name="country",v-model="demographics.country")
           div(class="column is-one-third")
             label(for="postalCode") Postal code
-            input(class="input", name="postalCode",v-model="input.postalCode")
+            input(class="input", name="postalCode",v-model="demographics.postalCode")
           div(class="column is-one-third")
-            input(class="checkbox", type="checkbox", name="noAddress",v-model="input.noAddress")
+            input(class="checkbox", type="checkbox", name="noAddress",v-model="demographics.noAddress")
             label(for="noAddress") No address
 
         div(class="columns")
           div(class="column is-one-third")
             label(for="phoneNumber") Phone number
-            input(class="input", name="phoneNumber",v-model="input.phoneNumber")
+            input(class="input", name="phoneNumber",v-model="demographics.phoneNumber")
           div(class="column is-one-third")
             label(for="email") Email address
-            input(class="input", name="email",v-model="input.email")
+            input(class="input", name="email",v-model="demographics.email")
           div(class="column is-one-third")
             label(for="occupation") Occupation/student
-            input(class="input", name="occupation",v-model="input.occupation")
+            input(class="input", name="occupation",v-model="demographics.occupation")
 
         div(class="columns")
           div(class="column is-one-third")
             label(for="phn") PHN
-            input(class="input", name="phn",v-model="input.phn")
+            input(class="input", name="phn",v-model="demographics.phn")
           div(class="column is-one-third")
             label(for="mrn") MRN
-            input(class="input", name="mrn",v-model="input.mrn")
+            input(class="input", name="mrn",v-model="demographics.mrn")
           div(class="column is-one-third")
             label(for="patientService") Patient service
-            input(class="input", name="patientService",v-model="input.patientService")
+            input(class="input", name="patientService",v-model="demographics.patientService")
 
         div(class="columns")
           div(class="column is-one-third")
             label(for="nextOfKin") Next of kin
-            input(class="input", name="genextOfKinnder",v-model="input.nextOfKin")
+            input(class="input", name="genextOfKinnder",v-model="demographics.nextOfKin")
           div(class="column is-one-third")
             label(for="nextOfKinRelationsip") Next of kin relationship
-            input(class="input", name="dateOfnextOfKinRelationsipBirth",v-model="input.nextOfKinRelationsip")
+            input(class="input", name="dateOfnextOfKinRelationsipBirth",v-model="demographics.nextOfKinRelationsip")
           div(class="column is-one-third")
             label(for="nextOfKinPhone") Next of kin phone
-            input(class="input", name="nextOfKinPhone",v-model="input.nextOfKinPhone")
+            input(class="input", name="nextOfKinPhone",v-model="demographics.nextOfKinPhone")
 
         div(class="columns")
           div(class="column is-one-third")
             label(for="decisionMakerName") Decision maker name
-            input(class="input", name="decisionMakerName",v-model="input.decisionMakerName")
+            input(class="input", name="decisionMakerName",v-model="demographics.decisionMakerName")
           div(class="column is-one-third")
             label(for="decisionMakerRelationship") Decision maker relationship
-            input(class="input", name="decisionMakerRelationship",v-model="input.decisionMakerRelationship")
+            input(class="input", name="decisionMakerRelationship",v-model="demographics.decisionMakerRelationship")
           div(class="column is-one-third")
             label(for="decisionMakerPhone") Decision make phone
-            input(class="input", name="decisionMakerPhone",v-model="input.decisionMakerPhone")
+            input(class="input", name="decisionMakerPhone",v-model="demographics.decisionMakerPhone")
 
 </template>
 
@@ -157,39 +157,97 @@ export default {
         { text: 'DNR C1', value: 'DNRC1' },
         { text: 'DNR C2', value: 'DNRC2' }
       ],
-      languages: [{ text: 'English', value: 'english' }],
-      input: {
-        familyName: 'Johns',
-        givenName: 'Erin',
-        middleName: '',
-        preferredName: 'Erin',
-        dateOfBirth: '09-SEP-1944',
-        personAge: '74',
-        gender: 'female',
-        martialStatus: 'widowed',
-        codeStatus: 'N/A',
-        languagePrimary: 'english',
-        relegion: '',
-        indigenous: 'no',
-        streetAddress: '402 Willow St.',
-        city: 'Vancouver',
-        country: 'Canada',
-        postalCode: 'V3F 9R2',
-        noAddress: 'false',
-        phoneNumber: '778-555-2947',
-        email: '',
-        occupation: 'Retired',
-        phn: '123456789',
-        mrn: '',
-        patientService: '',
-        nextOfKin: 'Thomas Johns',
-        nextOfKinRelationsip: 'Son',
-        nextOfKinPhone: '604-555-9865',
-        decisionMakerName: 'Thomas John',
-        decisionMakerRelationship: 'Son',
-        decisionMakerPhone: '604-555-9865'
-      }
+      languages: [{ text: 'English', value: 'english' }]
     }
+  },
+  computed: {
+    disableActions() {
+      let enableEhrControls = this.$store.getters.enableEhrControls
+      return !enableEhrControls
+    },
+    demographics() {
+      let data = this.$store.getters['ehrData/mergedData'] || {}
+      let input = data.demographics || {}
+      return input
+    },
+    isStudent() {
+      return this.$store.getters['visit/isStudent']
+    }
+  },
+  methods: {
+    // clearInputs: function() {
+    //   this.inputs = {
+    //     name: '',
+    //     profession: '',
+    //     unit: '',
+    //     day: '0',
+    //     time: '',
+    //     notes: ''
+    //   }
+    //   this.errorList = []
+    // },
+    // validateInputs: function() {
+    //   var inputs = this.inputs
+    //   // console.log('validate the inputs', inputs.name, this.inputs.name)
+    //   inputs.name = inputs.name.trim()
+    //   inputs.notes = inputs.notes.trim()
+    //   inputs.unit = inputs.unit.trim()
+    //   inputs.profession = inputs.profession.trim()
+    //   inputs.day = inputs.day.trim()
+    //   inputs.time = inputs.time.trim()
+    //   var vm = this
+    //   function check(prop, msg) {
+    //     if (prop.length == 0) {
+    //       vm.errorList.push(msg)
+    //     }
+    //   }
+    //   check(this.inputs.name, 'Name is required')
+    //   check(this.inputs.profession, 'Profession is required')
+    //   check(this.inputs.unit, 'Unit is required')
+    //   check(this.inputs.day, 'Day is required')
+    //   check(this.inputs.time, 'Time is required')
+    //   check(this.inputs.notes, 'Notes are required')
+    //   return this.errorList.length === 0
+    // },
+    // showDialog: function() {
+    //   this.clearInputs()
+    //   var today = moment().format('DD MMM')
+    //   var time = moment().format('HH:mm')
+    //   console.log('date is ', today, time)
+    //   if (this.populate) {
+    //     var inputs = this.inputs
+    //     inputs.name = this.username
+    //     inputs.notes = getPhrase(14)
+    //     inputs.profession = 'Nurse'
+    //     inputs.unit = 'ER'
+    //     inputs.day = today
+    //     inputs.time = time
+    //   }
+    //   this.showModal = true
+    // },
+    // cancelDialog: function() {
+    //   this.clearInputs()
+    //   this.showModal = false
+    // },
+    // saveDialog: function() {
+    //   if (this.validateInputs()) {
+    //     this.showModal = false
+    //     // console.log('Saving Progress Notes', this.inputs)
+    //     // We wish to send a modified object to the API server and not directly update our client side copy.
+    //     // Because the data is stored in our Vuex store we need to make a deep clone to prevent this error:
+    //     // "Do not mutate vuex store state outside mutation handlers."
+    //     let data = this.$store.getters['ehrData/assignmentData'] || {}
+    //     var modifiedValue = data.progressNotes || []
+    //     modifiedValue = modifiedValue ? JSON.parse(JSON.stringify(modifiedValue)) : []
+    //     modifiedValue.push(this.inputs)
+    //     // Prepare a payload to tell the API which property inside the assignment data to change
+    //     let payload = {
+    //       property: 'progressNotes',
+    //       value: modifiedValue
+    //     }
+    //     this.$store.dispatch('ehrData/sendAssignmentDataUpdate', payload)
+    //   }
+    // }
   }
 }
 </script>
--- database/config/lib/seed.js
+++ database/config/lib/seed.js
@@ -60,7 +60,7 @@ function doIntegrations () {
     }
   })
 
-  checkIntegration('assignments3').then((go) => {
+  checkIntegration('assignments5').then((go) => {
     if (go) {
       require('../seed-data/assignments')(true)
     }
