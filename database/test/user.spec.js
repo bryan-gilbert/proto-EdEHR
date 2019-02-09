4064e8f7419c2f99aa12491850e80aea7181a460
Bryan Gilbert <bryan.gilbert@memsharp.com>
2019-02-09 07:51:10 -0800

    unit testing User model

--- database/test/user.spec.js
+++ database/test/user.spec.js
@@ -1,43 +1,74 @@
-var should = require('should')
-const mongoose = require('mongoose')
-// var User = require('../models/user')
-import User from '../models/user'
-
-mongoose
-.connect(
-  'mongodb://localhost:27018/docker-node-mongo',
-  { useNewUrlParser: true }
-)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
+var should = require("should");
+const mongoose = require("mongoose");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+import User from "../models/user";
 
 /* global describe it */
-describe('user', function () {
-  it('should be invalid if id is empty', function (done) {
-    console.log('User is ', typeof User)
-    var m = new User()
-    m.validate(function (err) {
-      should.exist(err)
-      done()
-    })
-  })
-  it('can save one ', function (done) {
+describe("user mongoose schema testing", function() {
+  before(function(done) {
+    mongoose.connect(
+      "mongodb://localhost:27018/unittest",
+      { useNewUrlParser: true }
+    );
+    const db = mongoose.connection;
+    db.on("error", console.error.bind(console, "connection error"));
+    db.once("open", function() {
+      console.log("We are connected to test database!");
+      done();
+    });
+  });
+
+  after(function(done) {
+    console.log("After");
+    let clear = false;
+    if (clear) {
+      console.log("Clear test database!");
+      mongoose.connection.db.dropDatabase(function() {
+        mongoose.connection.close(function() {
+          console.log("Close test database!");
+          done();
+        });
+      });
+    } else {
+      mongoose.connection.close(function() {
+        console.log("Close test database!");
+        done();
+      });
+    }
+  });
+
+  it("should be invalid if id is empty", function(done) {
+    var m = new User();
+    m.validate(function(err) {
+      should.exist(err);
+      done();
+    });
+  });
+  it("can save one ", function(done) {
     const newUser = new User({
-      userId: '1234'
-    })
-    newUser.save()
-    .then(() => {
-      done()
-    })
-    .catch( () => {
-      done()
-    })
-  })
-  it.skip('can find one ', function(done) {
-    User.findOne({userId: '1234'}, function (err, doc) {
-      should.exist(doc)
-      should.not.exist(err)
-      done()
-    })
-  })
-})
+      toolConsumer: new ObjectID("56955ca46063c5600627f393"),
+      user_id: "1234"
+    });
+    newUser
+      .save()
+      .then(() => {
+        console.log("Saved user");
+        done();
+      })
+      .catch(err => {
+        console.log("Error ", err);
+        done();
+      });
+  });
+  it("can find one ", function(done) {
+    User.findOne({ user_id: "1234" }, function(err, doc) {
+      // console.log('results', err, doc)
+      should.exist(doc);
+      should.not.exist(err);
+      done();
+    }).catch(e => {
+      console.log("find one error", e);
+    });
+  });
+});
